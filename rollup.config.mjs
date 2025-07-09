110b1cb550e05c0f044d848d17350deb543dca9b
Yahweasel <git-rpo@yahweasel.com>
2025-07-09 20:37:56 +0000

    Modernizing build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,22 +2,34 @@ import * as fs from "fs/promises";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 
+const terserPlugin = [terser({
+    format: {
+        preamble: await fs.readFile("src/license.js", "utf8")
+    }
+})];
+
 export default {
     input: "src/main.ts",
     output: [
         {
+            file: "dist/weasound.mjs",
+            format: "es"
+        },
+        {
+            file: "dist/weasound.min.mjs",
+            format: "es",
+            plugins: terserPlugin
+        },
+        {
             file: "dist/weasound.js",
             format: "umd",
             name: "Weasound"
-        }, {
+        },
+        {
             file: "dist/weasound.min.js",
             format: "umd",
             name: "Weasound",
-            plugins: [terser({
-                format: {
-                    preamble: await fs.readFile("src/license.js", "utf8")
-                }
-            })]
+            plugins: terserPlugin
         }
     ],
     context: "this",
